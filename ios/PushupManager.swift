98d061188f616d2f59d815293d92bd873051183f
kimtaejin3 <rlaxowls1316@naver.com>
2025-09-13 15:41:04 +0900

    feat: ar 기능 제거 pushup 카운팅 기능만 하도록 수정

--- ios/PushupManager.swift
+++ ios/PushupManager.swift
@@ -6,78 +6,35 @@ import UIKit
 class PushupManager: NSObject {
 
   private var pushupRecognition: PushupRecognition?
-  private var arView: ARSCNView?
-  private var backButton: UIButton?
+  private var arSession: ARSession?
+  private var timer: Timer?
 
   @objc func startPushupSession() {
     DispatchQueue.main.async {
-      guard let rootViewController = UIApplication.shared.windows.first?.rootViewController else {
-        print("Could not find root view controller")
-        return
-      }
-      
-      // Create AR view
-      let arView = ARSCNView(frame: rootViewController.view.bounds)
-      arView.automaticallyUpdatesLighting = true
+      // Create AR session (without view)
+      let arSession = ARSession()
       
       // Configure AR session
       let configuration = ARFaceTrackingConfiguration()
-      arView.session.run(configuration)
+      arSession.run(configuration)
       
       // Set up pushup recognition
       self.pushupRecognition = PushupRecognition()
-      arView.delegate = self.pushupRecognition
       
-      // Add AR view to view hierarchy
-      rootViewController.view.addSubview(arView)
-      self.arView = arView
+      // Set up delegate for AR session
+      arSession.delegate = self.pushupRecognition
       
-      // Add back button
-      let backButton = UIButton(type: .system)
-      backButton.setTitle("뒤로가기", for: .normal)
-      backButton.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-      backButton.setTitleColor(.white, for: .normal)
-      backButton.layer.cornerRadius = 20
-      backButton.frame = CGRect(x: 20, y: 40, width: 100, height: 40)
-      backButton.addTarget(self, action: #selector(self.stopPushupSession), for: .touchUpInside)
+      // Store session
+      self.arSession = arSession
       
-      arView.addSubview(backButton)
-      self.backButton = backButton
-      
-      // Add count display
-      let countLabel = UILabel()
-      countLabel.frame = CGRect(x: 0, y: 100, width: rootViewController.view.bounds.width, height: 60)
-      countLabel.textAlignment = .center
-      countLabel.font = UIFont.boldSystemFont(ofSize: 48)
-      countLabel.textColor = .white
-      countLabel.backgroundColor = UIColor.black.withAlphaComponent(0.3)
-      countLabel.text = "0"
-      countLabel.tag = 100 // Tag for identification
-      
-      arView.addSubview(countLabel)
-      
-      // Start updating count
-      Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [weak self] timer in
-        guard let self = self, let count = self.pushupRecognition?.pushupCount else {
-          timer.invalidate()
-          return
-        }
-        
-        if let countLabel = self.arView?.viewWithTag(100) as? UILabel {
-          countLabel.text = "\(count)"
-        }
-      }
-      
-      print("Pushup session started")
+      print("Pushup session started in background")
     }
   }
 
   @objc func stopPushupSession() {
     DispatchQueue.main.async {
-      self.arView?.session.pause()
-      self.arView?.removeFromSuperview()
-      self.arView = nil
-      self.backButton = nil
+      self.arSession?.pause()
+      self.arSession = nil
       self.pushupRecognition = nil
       print("Pushup session stopped")
     }

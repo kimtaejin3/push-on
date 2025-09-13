00e3339b16e14d14675eef7d41a4fa393836bf78
kimtaejin3 <rlaxowls1316@naver.com>
2025-09-13 21:39:30 +0900

    refactor: Challenge 컴포넌트 분리

---
+++ src/screens/Challenge.tsx
@@ -0,0 +1,159 @@
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  NativeModules,
+} from 'react-native';
+import {encouragements, firstEncouragement} from '../constants/pushUp';
+
+const {PushupManager} = NativeModules;
+
+function Challenge(): React.JSX.Element {
+  const [count, setCount] = useState(0);
+  const [isTracking, setIsTracking] = useState(false);
+  const [encouragement, setEncouragement] = useState('');
+  const prevCountRef = useRef(0);
+
+  const getRandomEncouragement = () => {
+    const randomIndex = Math.floor(Math.random() * encouragements.length);
+    return encouragements[randomIndex];
+  };
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (isTracking) {
+      PushupManager.startPushupSession();
+
+      interval = setInterval(async () => {
+        try {
+          const currentCount = await PushupManager.getPushupCount();
+
+          if (currentCount > prevCountRef.current) {
+            setEncouragement(getRandomEncouragement());
+          }
+
+          prevCountRef.current = currentCount;
+          setCount(currentCount);
+        } catch (error) {
+          console.error('Failed to get pushup count:', error);
+        }
+      }, 500);
+    } else if (interval) {
+      clearInterval(interval);
+      PushupManager.stopPushupSession();
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+      PushupManager.stopPushupSession();
+    };
+  }, [isTracking]);
+
+  const toggleTracking = () => {
+    setIsTracking(prev => !prev);
+    if (isTracking) {
+      setCount(0);
+      prevCountRef.current = 0;
+      setEncouragement('');
+    } else {
+      setEncouragement(firstEncouragement);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>SET 1</Text>
+
+        <Text style={styles.countText}>{count}</Text>
+        <Text style={styles.encouragementText}>{encouragement}</Text>
+
+        <Text style={styles.instructionText}>
+          {isTracking && '기기를 얼굴과 마주보게 바닥에 두세요'}
+        </Text>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            isTracking ? styles.stopButton : styles.startButton,
+          ]}
+          onPress={toggleTracking}>
+          <Text style={styles.buttonText}>
+            {isTracking ? '중지하기' : '시작하기'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  countText: {
+    fontSize: 80,
+    fontWeight: '400',
+    marginTop: 100,
+  },
+  countLabel: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    marginLeft: 5,
+  },
+  button: {
+    paddingVertical: 20,
+    paddingHorizontal: 80,
+    borderRadius: 15,
+    marginBottom: 30,
+    width: '100%',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  startButton: {
+    backgroundColor: '#FF6969',
+  },
+  stopButton: {
+    backgroundColor: '#F44336',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: '100%',
+  },
+  instructionText: {
+    marginTop: 'auto',
+    marginBottom: 10,
+    fontSize: 16,
+    textAlign: 'center',
+    opacity: 0.7,
+  },
+  encouragementText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#FF6969',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+});
+
+export default Challenge;

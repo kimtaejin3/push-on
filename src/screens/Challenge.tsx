4656a09412257a9c68f7f36676b02cb90d915e20
kimtaejin3 <rlaxowls1316@naver.com>
2025-09-14 11:28:18 +0900

    design: 7일간 푸쉬업 통계 그래프 UI 구현

--- src/screens/Challenge.tsx
+++ src/screens/Challenge.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import usePushUpManager from '../hooks/usePushUpManager';
-import CustomButton from '../components/CustomButton';
+import CustomButton from '../components/common/CustomButton';
 import {useNavigation} from '@react-navigation/native';
 
 function Challenge(): React.JSX.Element {
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,22 +1,58 @@
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useNavigation} from '@react-navigation/native';
-import {StyleSheet, Text, View} from 'react-native';
-import CustomButton from '../components/CustomButton';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import CustomButton from '../components/common/CustomButton';
+import {useState} from 'react';
+
+const graphData = [
+  {id: 'sixDaysAgo', height: 100, date: '6일전'},
+  {id: 'fiveDaysAgo', height: 150, date: '5일전'},
+  {id: 'fourDaysAgo', height: 80, date: '4일전'},
+  {id: 'threeDaysAgo', height: 120, date: '3일전'},
+  {id: 'twoDaysAgo', height: 90, date: '2일전'},
+  {id: 'yesterday', height: 140, date: '1일전'},
+  {id: 'today', height: 170, highlight: true, date: '오늘'},
+];
 
 function Home(): React.JSX.Element {
   const navigation = useNavigation();
+
+  const [selectedBarIndex, setSelectedBarIndex] =
+    useState<(typeof graphData)[0]['id']>('today');
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Text style={styles.title}>한번에 챌린지</Text>
-      <View style={styles.card}>
-        <Text style={styles.cardTitle}>최고 기록</Text>
-        <Text style={styles.cardText}>
-          총 횟수: 40번 / 총 수행시간: 1분 40초
-        </Text>
-        <CustomButton
-          title="챌린지 시작하기"
-          onPress={() => navigation.navigate('Challenge')}
-        />
+      <View>
+        <Text style={styles.title}>나의 푸쉬업 데이터</Text>
+        <View style={styles.graphContainer}>
+          {graphData.map((item, _) => (
+            <View style={styles.graphItem} key={item.date}>
+              <TouchableOpacity
+                key={item.id}
+                style={[
+                  styles.graph,
+                  {height: item.height},
+                  selectedBarIndex === item.id && styles.highlightGraph,
+                ]}
+                onPress={() => setSelectedBarIndex(item.id)}
+              />
+              <Text style={styles.graphDate}>{item.date}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+      <View style={styles.challengeContainer}>
+        <Text style={styles.title}>한번에 챌린지</Text>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>최고 기록</Text>
+          <Text style={styles.cardText}>
+            총 횟수: 40번 / 총 수행시간: 1분 40초
+          </Text>
+          <CustomButton
+            title="챌린지 시작하기"
+            onPress={() => navigation.navigate('Challenge')}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -46,6 +82,36 @@ const styles = StyleSheet.create({
   cardText: {
     fontSize: 14,
     marginBottom: 20,
+  },
+  graphContainer: {
+    flexDirection: 'row',
+    gap: 15,
+    alignItems: 'flex-end',
+    height: 200,
+  },
+  graphItem: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: 10,
+  },
+  graph: {
+    width: '100%',
+    height: 200,
+    backgroundColor: '#EBEBEB',
+    borderRadius: 15,
+  },
+  graphDate: {
+    fontSize: 11,
+  },
+  highlightGraph: {
+    backgroundColor: '#FF6969',
+  },
+  selectedGraph: {
+    transform: [{scaleY: 1.2}],
+  },
+  challengeContainer: {
+    marginTop: 40,
   },
 });
 

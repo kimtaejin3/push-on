314f7f576643e21e6f3898a7396a2d8275d1a1e3
kimtaejin3 <rlaxowls1316@naver.com>
2025-10-11 20:19:26 +0900

    chore: 주석처리 해놓은 온보딩/인증 스크린 관련 코드 복구

--- src/navigations/AppNavigation.tsx
+++ src/navigations/AppNavigation.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import React, {useState} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
   createStaticNavigation,
   NavigatorScreenParams,
 } from '@react-navigation/native';
-
 import ChallengeScreen from '../screens/ChallengeScreen';
 import BottomTabNavigation, {BottomTabParamList} from './BottomTabNavigation';
 import SettingScreen from '../screens/SettingScreen';
-// import AuthScreen from '../screens/AuthScreen';
-// import OnboardingScreen from '../screens/OnboardingScreen';
-// import {useAuth} from '../hooks/useAuth';
-// import {useIsOnboarded} from '../hooks/useIsOnboarded';
+import AuthScreen from '../screens/AuthScreen';
+import OnboardingScreen from '../screens/OnboardingScreen';
+import {useAuth} from '../hooks/useAuth';
+import {useIsOnboarded} from '../hooks/useIsOnboarded';
 
 const AppStack = createNativeStackNavigator({
   initialRouteName: 'Tabs',
@@ -38,22 +37,22 @@ const AppNavigation = createStaticNavigation(AppStack);
 
 // 조건부 네비게이션 컴포넌트
 const ConditionalNavigation = () => {
-  // const {isLoggedIn, loading: authLoading} = useAuth();
-  // const {isOnboarded, loading: onboardingLoading} = useIsOnboarded();
+  const {isLoggedIn, loading: authLoading} = useAuth();
+  const {isOnboarded, loading: onboardingLoading} = useIsOnboarded();
 
-  // const [_, setRerender] = useState(false);
+  const [_, setRerender] = useState(false);
 
-  // if (authLoading || onboardingLoading) {
-  //   return null;
-  // }
+  if (authLoading || onboardingLoading) {
+    return null;
+  }
 
-  // if (!isLoggedIn) {
-  //   return <AuthScreen />;
-  // }
+  if (!isLoggedIn) {
+    return <AuthScreen />;
+  }
 
-  // if (isOnboarded === false) {
-  //   return <OnboardingScreen onComplete={() => setRerender(true)} />;
-  // }
+  if (isOnboarded === false) {
+    return <OnboardingScreen onComplete={() => setRerender(true)} />;
+  }
 
   // 로그인되고 온보딩도 완료된 경우 메인 네비게이션 표시
   return <AppNavigation />;
